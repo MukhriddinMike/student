e55f32103cc7f1dae84fde35e988680b12bbac7e
mukhriddinmike <miwady@mail.ru>
2019-08-04 10:57:29 +0900

    Plotted all but not working in ubuntu : UserWarning: Matplotlib is currently using agg, which is a non-GUI backend, so cannot show the figure.   plt.show()

---
+++ knn_charm.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.utils import shuffle
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import linear_model, preprocessing
+
+data = pd.read_csv('car.data')
+print(data.head())
